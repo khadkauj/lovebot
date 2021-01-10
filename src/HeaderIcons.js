0dd37b400e68199ec2c4310578ab0c8f8d079e9f
ujjwal khadka <khadkauj@gmail.com>
2021-01-10 22:00:25 +0100

    jan 10

--- src/HeaderIcons.js
+++ src/HeaderIcons.js
@@ -5,15 +5,32 @@ import { IconButton } from '@material-ui/core';
 import './HeaderIcon.css'
 import { Link } from "react-router-dom";
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { useDispatch } from 'react-redux';
+import { Sign_out } from './features/user/userSlice';
 
 const HeaderIcons = ({backarrow_prop}) => {
+
+    const dispatch = useDispatch()
+
+    const sign_out = (e) => {
+        
+
+        dispatch(Sign_out());
+
+    }
+
+
+
+
+
+
     return (
         <div className='header'>
 
            {backarrow_prop ?
                     // <Link to ='/'>   </Link>
                ( <Link to ='/after_signin_and_imageupload'><IconButton className="header__icon"><ArrowBackIosIcon fontSize='large'></ArrowBackIosIcon> </IconButton></Link>):
-               (<IconButton className="header__icon"><AccountCircleIcon fontSize='large' /></IconButton>)}
+               (<IconButton className="header__icon"><AccountCircleIcon onClick={sign_out} fontSize='large' /></IconButton>)}
        
                 <Link to ='/after_signin_and_imageupload'><IconButton className="header__icon"><img className='header_icon_img' src='https://dbdzm869oupei.cloudfront.net/img/sticker/preview/4788.png' alt='' /></IconButton> </Link>
                 {/* // <Link to='/chats'> </Link> */}
